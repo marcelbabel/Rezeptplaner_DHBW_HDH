abbe4f12c97c2d08dfc0c82aea2673f148a1adef
marcelbabel <babelmarcel5@gmail.com>
2026-03-10 10:06:37 +0100

    weiß ned

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+
+// DEBUG-LOGS
+console.log("--- DEBUG START ---");
+console.log("Aktuelles Verzeichnis:", process.cwd());
+console.log("Gelesene URI aus .env:", process.env.MONGO_URI);
+console.log("--- DEBUG ENDE ---");
 
 const app = express();
 app.use(express.json());
 
+
 const recipeRoutes = require('./routes/recipes');
 app.use('/api/recipes', recipeRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+
+const dbUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/rezeptplaner';
+
+mongoose.connect(dbUri)
   .then(() => console.log('MongoDB verbunden'))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error('Verbindung fehlgeschlagen:', err));
 
 app.get('/', (req, res) => {
   res.send('API läuft');
